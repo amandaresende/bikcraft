51fec2cef765ad26d4e9b3bf227c5597c9061f2b
Amanda Resende <amandacamposresende@gmail.com>
2022-11-21 14:52:03 -0300

    Otimizações nos orçamentos e seguros

---
+++ js/script.js
@@ -0,0 +1,23 @@
+const links = document.querySelectorAll(".header-menu a");
+
+function ativarLink(link) {
+  const url = location.href
+  const href = link.href
+
+  if(url.includes(href)){
+    link.classList.add("ativo")
+  }
+}
+links.forEach(ativarLink);
+
+const parametros = new URLSearchParams(location.search)
+
+function ativarProduto(parametro){
+  const elemento = document.getElementById(parametro)
+  if(elemento) {
+     elemento.checked = true
+  }
+}
+
+parametros.forEach(ativarProduto)
+
